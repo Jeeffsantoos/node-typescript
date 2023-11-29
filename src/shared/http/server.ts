66355a81d05c9a89161cf3dab2d2694083460e45
Jefferson Lima <jeeffsantoos@outlook.com>
2023-11-29 00:27:41 -0300

    [fix] - adicionando o celebrate e aceitando tratamento de exceções

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,17 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/http/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
