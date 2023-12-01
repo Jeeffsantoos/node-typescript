dc31225445eddb593dd6d310a5c1acf3efae7d68
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-01 18:52:15 -0300

    [feat] - Create user service

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../typeorm/repositories/UsersRepository';
+import AppError from '@shared/http/errors/AppError';
+import User from '../typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ email, name, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('There is already one user with this email');
+    }
+
+    const user = usersRepository.create({
+      name,
+      password,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
