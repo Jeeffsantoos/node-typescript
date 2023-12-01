6cf0e04a7cd729b841fdecd5b7628c278ac153e5
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-01 19:43:53 -0300

    [feat] - user modules

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import AppError from '@shared/http/errors/AppError';
 import User from '../typeorm/entities/User';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
   name: string;
@@ -18,9 +19,11 @@ class CreateUserService {
       throw new AppError('There is already one user with this email');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
-      password,
+      password: hashedPassword,
       email,
     });
 
