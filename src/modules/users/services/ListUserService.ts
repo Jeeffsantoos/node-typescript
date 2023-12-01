3120a82df885685157b53eb6d79a5e82ff0737ef
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-01 18:56:11 -0300

    [feat] - list user service

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,20 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import AppError from '@shared/http/errors/AppError';
+
+class ListUserService {
+  public async execute(): Promise<User[]> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.find();
+
+    if (!user) {
+      throw new AppError('There is no user.');
+    }
+
+    return user;
+  }
+}
+
+export default ListUserService;
