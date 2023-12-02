6085ca6ee7689870a30f03d6e863f95f1f0bdb08
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-02 00:44:54 -0300

    [feat] - middleware auth

---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import { Response, Request, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing,');
+  }
+
+  const [type, token] = authHeader.split(' ');
+
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
