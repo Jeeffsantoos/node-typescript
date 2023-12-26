783d02fb743936b3511cf05e75ba8a164ab8100c
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-26 13:08:24 -0300

    - migrations

---
+++ src/shared/typeorm/migrations/1702699324666-AddOrderIdToOrdersProdutcs.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddOrderIdToOrdersProdutcs1702699324666
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductOrder',
+        columnNames: ['order_id'],
+        referencedTableName: 'orders',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrdersProductOrder');
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
