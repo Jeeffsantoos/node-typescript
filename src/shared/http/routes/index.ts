cbf5db5be7904f0e50b29a77a4e3b11d986d5cb6
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-26 13:08:09 -0300

    - feat routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,8 @@
+import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRoute from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
@@ -10,5 +13,7 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRoute);
-
+routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 export default routes;
