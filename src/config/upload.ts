bb85ca552932eb480c0e30ac22ed7c1d3eafae2e
Jefferson Lima <jeeffsantoos@outlook.com>
2023-12-26 13:05:39 -0300

    - email and avatar config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,45 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
+  driver: 'disk',
   directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
+  tmpFolder,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
 
-      const filename = `${fileHash}-${file.originalname}`;
+        const filename = `${fileHash}-${file.originalname}`;
 
-      callback(null, filename);
+        callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'api-vendas',
     },
-  }),
-};
+  },
+} as IUploadConfig;
